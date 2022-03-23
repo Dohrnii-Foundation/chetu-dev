716c2b9200177468de303b35e53451e6449a2c07
amit <amity3@chetu.com>
2022-03-23 20:11:22 +0530

    add login route

--- routes/authentication.js
+++ routes/authentication.js
@@ -7,6 +7,7 @@ const router = express.Router();
  */
 router.post("/seed", async (req, res, next) => {
   try {
+   // const options = req.body;
     const result = await authentication.generateRandomSeeds();
     res.status(result.status || 200).send(result);
   } catch (err) {
@@ -34,6 +35,45 @@ router.post("/saveSeedPhrase", async (req, res, next) => {
   try {
     const options = req.body;
     const result = await authentication.saveVerifiedSeedPhrase(options);
+    res.status(result.status || 200).send(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+/**
+ * Create Password.
+ */
+ router.post("/create/password", async (req, res, next) => {
+  try {
+    const options = req.body;
+    const result = await authentication.createPassword(options);
+    res.status(result.status || 200).send(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+/**
+ * Login.
+ */
+ router.post("/login", async (req, res, next) => {
+  try {
+    const options = req.body;
+    const result = await authentication.login(options);
+    res.status(result.status || 200).send(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+/**
+ * Change Password.
+ */
+ router.post("/change/password", async (req, res, next) => {
+  try {
+    const options = req.body;
+    const result = await authentication.changePassword(options);
     res.status(result.status || 200).send(result);
   } catch (err) {
     next(err);
